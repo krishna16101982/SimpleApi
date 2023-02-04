f371fa6f868b81e22e62786ac9287e85fd1c3209
kumar-gaurav529 <kumar_gaurav529@yahoo.com>
2023-02-05 00:15:20 +0530

    secondCommit

--- SimpleAPI/Controllers/ValuesController.cs
+++ SimpleAPI/Controllers/ValuesController.cs
@@ -24,7 +24,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "Les Jackson";
         }
 
         // POST api/values
